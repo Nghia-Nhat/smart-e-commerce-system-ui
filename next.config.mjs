f6f8ec2eb7bc32dd7b34d2cb4c808917e7cfff61
ndang21 <nhatcong.dang2@dxc.com>
2024-07-28 11:38:53 +0700

    Accept all domain of image

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'assets.myntassets.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'cdn.dummyjson.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'fakestoreapi.com',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
       },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
 };
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export default nextConfig;
